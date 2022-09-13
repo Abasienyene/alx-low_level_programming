568ceca437d39b72010d827192dc22f5a1b0a231
Abasienyene <abasonform@gmail.com>
2022-09-13 06:49:38 -0700

    last

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+	char ch;
 
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
