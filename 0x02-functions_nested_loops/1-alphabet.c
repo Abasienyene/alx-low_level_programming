11169af6eb502b4624660f70a79de1d4719d19a4
Abasienyene <abasonform@gmail.com>
2022-09-13 08:06:44 -0700

    teet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,14 @@
 #include "main.h"
-
 /**
-
  *print_alphabet - prints the alphabet, in lowercase, followed by a new line
-
  */
 
 void print_alphabet(void)
 
 {
-
 	char ch;
 
-	ch = 'a';
+	iiiich = 'a';
 
 	while (ch <= 'z')
 
