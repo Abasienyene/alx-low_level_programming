e838878fd6d8b8c3b5a192d5cbd81947700a6d0a
Abasienyene <abasonform@gmail.com>
2022-09-13 11:59:28 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,18 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-
- * _islower - checks for lowercase character
-
- * @c: the character to check
-
- * Return: 1 if c is lowercase, 0 otherwise
-
+ * _islower - main function
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if true, 0 if false
+ *
  */
 
 int _islower(int c)
-
 {
-
-	return (c >= 'a' && c <= 'z');}
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
