6c93f0fb4e37424b0615b69ddbae9c25a25ac790
Abasienyene <abasonform@gmail.com>
2022-09-13 12:21:35 -0700

    3 colours, 10 digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "6-abs.c"
+/**
+ * print_last_digit - main function
+ *
+ * @n: integer to get last digit of
+ * Return: last digit of n
+ *
+ */
+
+int print_last_digit(int n)
+{
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
+}
