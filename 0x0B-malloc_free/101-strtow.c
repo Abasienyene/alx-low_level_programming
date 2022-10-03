6b5708b875893a720f92416bdddb689621a46b22
Abasienyene <abasonform@gmail.com>
2022-10-03 17:28:19 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -33,7 +33,8 @@ int count_word(char *s)
  *
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
- */char **strtow(char *str)
+ */
+char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
